904b66bbfdea1f85e8c09c63e004941b23077614
Noud de Brouwer <noud4@home.nl>
2019-07-16 20:20:59 +0200

    Steps 0, 1 and 2.

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+
+export default () => (
+  	<div style={{ color: `teal` }}>
+	    <Header headerText="About Gatsby" /> 
+	    <p>Such wow. Very React.</p>
+	</div>
+)
